3f850591378496485b98cda860ed9f48222c3538
s82626 <s82626@informatik.htw-dresden.de>
2022-04-12 20:04:13 +0200

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -55,6 +55,7 @@ class App{
         speechElement.continous = true;
         var final_transcript = '';
 
+
         speechElement.start();
 
 
@@ -68,24 +69,23 @@ class App{
             console.log('Speech recognition activated.');
         };
 
-        // speechElement.onresult = function(event) {  
-        //     var interim_transcript = '';
-        //     for (var i = event.resultIndex; i < event.results.length; ++i) {
-        //         if (event.results[i].isFinal) {
-        //             final_transcript += event.results[i][0].transcript;
-        //             if (final_transcript[i][0] == 'create') {	
-        //                 const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-        //                 const mesh = new THREE.Mesh( self.geometry, material );
-        //                 mesh.position.set(0,0,-0.3).applyMatrix4( controller.matrixWorld );
-        //                 mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
-        //                 self.scene.add( mesh );
-        //                 self.meshes.push( mesh );
-        //             }
-        //         } else {
-        //             interim_transcript += event.results[i][0].transcript;
-        //         }
-        //     }
-        // }
+        speechElement.onresult = function(event) {  
+            var interim_transcript = '';
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    if (event.results[i][0].transcript.indexOf('create') == 1) {	
+                        const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
+                        const mesh = new THREE.Mesh( self.geometry, material );
+                        mesh.position.set(0,0,-0.3).applyMatrix4( controller.matrixWorld );
+                        mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
+                        self.scene.add( mesh );
+                        self.meshes.push( mesh );
+                    }
+                } else {
+                    interim_transcript += event.results[i][0].transcript;
+                }
+            }
+        }
 
         function onSelect() {
             const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
