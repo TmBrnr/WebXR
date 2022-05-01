1888e63442b3c5e7955a87cddaa62877a59e1fbd
Tim <72656043+TmBrnr@users.noreply.github.com>
2022-05-01 18:44:47 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,144 +1,166 @@
 import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three125/OrbitControls.js';
-import { Stats } from '../../libs/stats.module.js';
-import { ARButton } from '../../libs/ARButton.js';
 
+const {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  Mesh,
+  BoxGeometry,
+  MeshBasicMaterial,
+} = THREE;
 
-function record() {
-	this.recognition = new webkitSpeechRecognition();
-	    // settings
-			this.recognition.continuous = false; // stop automatically
-			this.recognition.interimResults = true;
-			this.recognition.lang = 'en-US';
-
-		    this.startCapture = function() {
-		      this.recognition.start();
-		    }
-
-		    this.stopCapture = function() {
-		      this.recognition.stop();
-		    }
-
-		    this.recognition.onresult = function(event) {
-		      console.log(event.results[0][0].transcript);
-		      //$('#output').text(event.results[0][0].transcript);
-			alert("worked");
-		    }
-
-		    this.recognition.onerror = function(event) {
-		      console.log(event.error);
-		    }
-
-		    console.log("webkitSpeechRecognition is available.");
-		  }
-
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-        
-        this.clock = new THREE.Clock();
-        
-		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
-		
-		this.scene = new THREE.Scene();
-        
-		this.scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
-
-        const light = new THREE.DirectionalLight( 0xffffff );
-        light.position.set( 1, 1, 1 ).normalize();
-		this.scene.add( light );
-			
-		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
-		this.renderer.setPixelRatio( window.devicePixelRatio );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
-        
-		container.appendChild( this.renderer.domElement );
-        
-        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-        this.controls.target.set(0, 3.5, 0);
-        this.controls.update();
-        
-        this.stats = new Stats();
-        document.body.appendChild( this.stats.dom );
-        
-        this.initScene();
-        this.setupXR();
-        
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    initScene(){
-        this.geometry = new THREE.BoxBufferGeometry( 0.06, 0.06, 0.06 ); 
-        this.meshes = [];
-    }
-    
-    setupXR(){
-        var speechElement = new webkitSpeechRecognition();
-        
-
-        speechElement.start();
-
-
-        this.renderer.xr.enabled = true;
-
-        const self = this;
-        let controller;
-
-
-        speechElement.onstart = function() {
-            console.log('Speech recognition activated.');
-        };
-
-        speechElement.onresult = function(event) {  
-            var interim_transcript = '';
-            for (var i = event.resultIndex; i < event.results.length; ++i) {
-                if (event.results[i].isFinal) {
-                    event.results[i][0].transcript;
-			alert();
-                } else {
-                    interim_transcript += event.results[i][0].transcript;
-			alert("Went second");
-                }
-            }
-    	}
-
-	speechElement.recognition.onerror = function(event) {
-      		alert("Error");
-        }
-	
-	   
-	    
-        function onSelect() {
-            const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-		    const mesh = new THREE.Mesh( self.geometry, material );
-		    mesh.position.set(0,0,-0.3).applyMatrix4( controller.matrixWorld );
-		    mesh.quaternion.setFromRotationMatrix( controller.matrixWorld ); 
-		    self.scene.add( mesh ); 
-		    self.meshes.push( mesh );
-		}
-	
-        const btn = new ARButton( this.renderer );
-
-        controller = this.renderer.xr.getController(0);
-        controller.addEventListener('select', onSelect);
-        this.scene.add(controller);
-
-        this.renderer.setAnimationLoop( this.render.bind(this) );
-    }
-    
-    resize(){
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize( window.innerWidth, window.innerHeight );  
-    }
-    
-	render( ) {   
-        this.stats.update();
-        this.meshes.forEach( (mesh) => { mesh.rotateY( 0.01 ); });
-        this.renderer.render( this.scene, this.camera );
-    }
+function createWebGlContext(glAttribs) {
+  const webGlCanvas = document.createElement("canvas");
+  const context = webGlCanvas.getContext("webgl2", glAttribs);
+  return context;
 }
 
-export { App };
+function makeCube() {
+  const geometry = new BoxGeometry(1,1,1);
+  const material = new MeshBasicMaterial({color:"blue"});
+  const cube = new Mesh(geometry, material);
+  return cube;
+}
+
+function makeOutputCanvas() {
+  const outputCanvas = document.createElement("canvas");
+  outputCanvas.height = window.innerHeight;
+  outputCanvas.width = window.innerWidth;
+  const outputContext = outputCanvas.getContext("xrpresent");
+  return {outputCanvas, outputContext};
+}
+
+function createThreeScene(webGlContext) {
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+      75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const renderer = new WebGLRenderer({
+    context: webGlContext
+  });
+  
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  
+  const cube = makeCube();
+  scene.add(cube);
+  
+  function renderScene(delta) {
+    renderer.render(scene, camera);
+  }
+  
+  return {
+    scene, camera, renderer, renderScene };
+}
+
+const globalShared = {
+  getRefFrameFor(session) {
+    return session.exclusive
+        ? this.exclusiveRefFrame
+        : this.nonExclusiveRefFrame;
+  }
+};
+function ensureShared(session) {
+  const shared = globalShared;
+  
+  if (!shared.initialized) {
+    shared.webGlContext = createWebGlContext({
+      compatibleXRDevice: session.device
+    });
+  
+    const {
+      scene, camera, renderer, renderScene
+    } = createThreeScene(shared.webGlContext);
+    
+    shared.scene = scene;
+    shared.camera = camera;
+    shared.renderer = renderer;
+    shared.renderScene = renderScene;
+    
+    shared.initialized = true;
+  }
+  
+  return shared;
+}
+
+function onSessionStarted(session) {
+  const shared = ensureShared(session);
+  const { webGlContext } = shared;
+  
+  session.baseLayer = new XRWebGLLayer(session, webGlContext);
+  
+  return session.requestFrameOfReference("eyeLevel").then(refFrame => {
+    // Share the reference frame.
+    // Depending on whether or not the session is a magic window,
+    // store the reference frame in different locations.
+    if (session.exclusive) {
+      shared.exclusiveRefFrame = refFrame;
+    } else {
+      shared.nonExclusiveRefFrame = refFrame;
+    }
+
+    session.requestAnimationFrame(onXrFrame);
+  });
+}
+
+function onXrFrame(delta, frame) {
+  const session = frame.session;
+
+  const shared = ensureShared();
+  const refFrame = shared.getRefFrameFor(session);
+  const {
+    renderScene,
+  } = shared;
+  
+  renderScene(delta);
+  session.requestAnimationFrame(onXrFrame);
+}
+
+function run() {
+  initXr().catch(err => {
+    document.write(JSON.stringify({
+      name: err.name,
+      message: err.message,
+      fileName: err.fileName,
+      lineNumber: err.lineNumber,
+      columnNumber: err.columnNumber,
+    }));
+    console.error(err);
+  });
+}
+
+//run();
+
+document.querySelector(".enter-xr").addEventListener("click", event => {
+  navigator.xr.requestDevice()
+    .then(device => device.requestSession({ exclusive:true }))
+    .then(session => onSessionStarted(session));
+  
+  //run();
+});
+
+
+
+function testScene() {
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+      75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const renderer = new WebGLRenderer();
+  const canvas = renderer.domElement;
+  document.body.appendChild(canvas);
+  
+  camera.position.z += 5;
+  camera.position.y += 2;
+  camera.position.x += 1;
+  
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  
+  const cube = makeCube();
+  scene.add(cube);
+  
+  function renderScene(delta) {
+    renderer.render(scene, camera);
+  }
+  
+  renderScene();
+}
+
+//testScene();
